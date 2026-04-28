f0ff4a7a3f2657af73234b6e38822d8f90fc3428
Vishal Singh Yadav <vsy9599@gmail.com>
2026-04-29 01:05:33 +0530

    Vishal Added

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello World from Flask 🚀"
+    return "Hello World from Flask Vishal"
 
 if __name__ == "__main__":
     app.run(debug=True)
