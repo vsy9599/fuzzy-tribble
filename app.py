819b9bdcd75863314959f85c1bb37571e341fd53
Vishal Singh Yadav <vsy9599@gmail.com>
2026-04-29 14:19:50 +0530

    update typo!

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello World from git and local repsdfsdfsository!"
+    return "Hello World from git and local!"
 
 if __name__ == "__main__":
     app.run(debug=True)
